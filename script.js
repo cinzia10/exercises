9b52af1c971ddff0fbaed6e68c13fa52a9b38a97
LittleAkira <cinzia.ariotti.10@gmail.com>
2022-05-06 11:15:23 +0200

    add

---
+++ script.js
@@ -0,0 +1,37 @@
+//// CICLI E LOOP
+
+const list = [23, 13, 34, 56, 99, 101]
+const newList = [];
+
+//// STAMPARE IL NUMERO 56
+// for (const element of list) {
+//     if (element === 56) {
+//         newList.push(element)
+//     }
+// }
+
+//// STAMPARE I NUMERI SUCCESSIVI AL 56
+// for (const element of list) {
+//     if (element > 56){
+//         newList.push(element)
+//     }
+// }
+
+
+//// STAMPARE TUTTI I NUMERI TRANNE IL 56
+// for (const number of list) {
+//     if (number !== 56) {
+//         newList.push(number)
+//     }
+// }
+
+
+//// STAMPARE SOLO I NUMERI DISPARI
+// for (const number of list) {
+//     if (number%2!==0){
+//         newList.push(number)
+//     }
+// } 
+
+
+console.log(newList)
